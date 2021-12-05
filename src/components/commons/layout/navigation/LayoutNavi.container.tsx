0b3cd71106ad3c74b86fc8e14153f5f8b58c5b35
moogieon <vzlzydn5007@gmail.com>
2021-12-05 23:48:50 +0900

    fix-onClick

--- src/components/commons/layout/navigation/LayoutNavi.container.tsx
+++ src/components/commons/layout/navigation/LayoutNavi.container.tsx
@@ -1,5 +1,21 @@
 import NaviWrapUI from "./LayoutNavi.presenter";
-
+import { useRouter } from "next/router";
 export default function NaviWrap() {
-  return <NaviWrapUI />;
+  const router = useRouter();
+  const onClickHome = () => {
+    router.push("../plaskHome");
+  };
+  const onClickList = () => {
+    router.push("../plaskList");
+  };
+  const onClickCart = () => {
+    router.push("../plaskCart");
+  };
+  return (
+    <NaviWrapUI
+      onClickHome={onClickHome}
+      onClickList={onClickList}
+      onClickCart={onClickCart}
+    />
+  );
 }
--- src/components/commons/layout/navigation/LayoutNavi.presenter.tsx
+++ src/components/commons/layout/navigation/LayoutNavi.presenter.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import {
   NaviWrap,
   Logo,
@@ -8,33 +7,22 @@ import {
   NaviBtn,
 } from "./LayoutNavi.styles";
 
-export default function NaviWrapUI() {
-  const router = useRouter();
-  const onClickHome = () => {
-    router.push("../plaskHome");
-  };
-  const onClickList = () => {
-    router.push("../plaskList");
-  };
-  const onClickCart = () => {
-    router.push("../plaskCart");
-  };
-
+export default function NaviWrapUI(props) {
   return (
     <NaviWrap>
-      <Logo src="./icon/logo.svg" onClick={onClickHome} />
+      <Logo src="./icon/logo.svg" onClick={props.onClickHome} />
 
       <MenuBox>
-        <Menu onClick={onClickList}>New Releases</Menu>
-        <Menu onClick={onClickList}>Men</Menu>
-        <Menu onClick={onClickList}>Women</Menu>
-        <Menu onClick={onClickList}>Kids</Menu>
-        <Menu onClick={onClickList}>Sale</Menu>
+        <Menu onClick={props.onClickList}>New Releases</Menu>
+        <Menu onClick={props.onClickList}>Men</Menu>
+        <Menu onClick={props.onClickList}>Women</Menu>
+        <Menu onClick={props.onClickList}>Kids</Menu>
+        <Menu onClick={props.onClickList}>Sale</Menu>
       </MenuBox>
 
       <NaviBtnBox>
         <NaviBtn src="./icon/Heart.svg" />
-        <NaviBtn src="./icon/Cart.svg" onClick={onClickCart} />
+        <NaviBtn src="./icon/Cart.svg" onClick={props.onClickCart} />
       </NaviBtnBox>
     </NaviWrap>
   );
